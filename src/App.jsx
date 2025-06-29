680931808245467407b454f864d4519254e81dbd
Konrad <konrad-landzberg@wp.pl>
2025-06-29 15:08:59 +0200

    Initial project config

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
